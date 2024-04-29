14b0135f5b9dfc1f6f7a38564be36c6a315086b4
Matthew Tran <Matthew.H.Tran-1@ou.edu>
2024-04-29 15:52:18 -0500

    Button Control Functions (*Needs implementation and Review*)

--- src/main/java/com/calorieminder/calorieminder/HelloController.java
+++ src/main/java/com/calorieminder/calorieminder/HelloController.java
@@ -1,7 +1,14 @@
 package com.calorieminder.calorieminder;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
@@ -11,4 +18,16 @@ public class HelloController {
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to CalorieMinder!");
     }
+
+    @FXML
+    protected void moveToMainPage(ActionEvent event) throws IOException {
+        FXMLLoader main = new FXMLLoader(getClass().getResource("CalorieMinderIntroPage.fxml"));
+        Scene scene = new Scene(main.load(), 320, 240);
+        Stage currentWindow = (Stage)((Node)event.getSource()).getScene().getWindow();
+        currentWindow.setScene(scene);
+        currentWindow.show();
+    }
+
+    //Possible transition function to cater to all pages rather than make multiple methods for one
+    //protected void transitionToPage (ActionEvent event) throws IO Exception {}
 }
