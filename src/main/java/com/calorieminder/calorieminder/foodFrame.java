b5a4d98c546170060d0196676a3368b7204da6c8
Cale Stringfellow <cstring1028@gmail.com>
2024-04-29 18:18:01 -0500

    Added navigation from food page to main page.

--- src/main/java/com/calorieminder/calorieminder/foodFrame.java
+++ src/main/java/com/calorieminder/calorieminder/foodFrame.java
@@ -10,8 +10,8 @@ import java.io.IOException;
 public class foodFrame extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(foodFrame.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(foodFrame.class.getResource("foodPage.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
