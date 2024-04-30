e7ff12eef01f65ecdd621f0ef35e3bcb12e94b6e
JoshuaDick <Joshua.D.Dick-1@ou.edu>
2024-04-29 22:14:46 -0500

    Added JSON.Simple Dependency

--- src/main/java/com/calorieminder/calorieminder/Model/USDAApi.java
+++ src/main/java/com/calorieminder/calorieminder/Model/USDAApi.java
@@ -1,12 +1,13 @@
 package com.calorieminder.calorieminder.Model;
 
+import org.json.simple.parser.JSONParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Arrays;
-import org.json.simple.parser.JSONParser;
 
 //class for making API calls to USDA and retrieving data
 public class USDAApi {
